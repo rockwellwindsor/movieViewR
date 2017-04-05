8f189abb0fb89ade50d650718813231fe4d209c9
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-04-05 09:31:13 -0500

    Development
    Create Detail activity to display movie details when a user clicks on a movie thumbnail. Create utilities/NetworkUtils file to handle the network communications.  Added picasso to the gradle file to handle image loading and caching.
    
    Changes to be committed:
    	modified:   app/build.gradle
    	modified:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/java/com/example/android/windsordesignstudio/movieviewr/DetailActivity.java
    	new file:   app/src/main/java/com/example/android/windsordesignstudio/movieviewr/utilities/NetworkUtils.java
    	new file:   app/src/main/res/layout/activity_detail.xml

---
+++ app/src/main/java/com/example/android/windsordesignstudio/movieviewr/utilities/NetworkUtils.java
@@ -0,0 +1,12 @@
+package com.example.android.windsordesignstudio.movieviewr.utilities;
+
+/**
+ * Created by rockwellrice on 4/5/17.
+ *
+ *  This class will be responsible for communicating with the movie serice
+ *
+ */
+
+public class NetworkUtils {
+
+}
