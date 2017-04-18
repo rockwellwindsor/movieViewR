277561c1b06b3d9c9ecde3aa318c8d8479adecd0
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-04-18 08:50:12 -0500

    Development
    Added image poster into the detail page.
    
    Changes to be committed:
    	modified:   app/src/main/java/com/example/android/windsordesignstudio/movieviewr/DetailActivity.java
    	modified:   app/src/main/res/layout/activity_detail.xml

--- app/src/main/java/com/example/android/windsordesignstudio/movieviewr/DetailActivity.java
+++ app/src/main/java/com/example/android/windsordesignstudio/movieviewr/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.windsordesignstudio.movieviewr;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,17 +38,30 @@ public class DetailActivity extends AppCompatActivity {
 
         if (intentThatStartedThisActivity != null) {
             if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+
+                /*
+                * Getting the context to use picasso to display the image was a little confusing for me.
+                * This solution came from: http://stackoverflow.com/questions/28754499/how-to-get-context-in-an-intent-service
+                * and appeared to work, although admittedly I am a little unsure exactly how it solved the issue.
+                */
+
+                Context context = getApplicationContext();
+
                 mMovie = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+
                 try {
                     JSONArray jsonArray = new JSONArray(mMovie);
                     // Display title
                     mMovieTitle.setText("Title : " + jsonArray.getString(0));
+                    // Display the Poster
+                    // Using Picasso to set the poster image in the ImageView
+                    Picasso.with(context).load(jsonArray.getString(1)).resize(600, 900).into(mMovieImage);
                     // Display the Rating
                     mMovieRating.setText("Rating : " + jsonArray.getString(2));
                     // Display the plot synopsis
                     mMovieDescription.setText("Synopsis : " + jsonArray.getString(3));
                     // Display the release date
-                    mMovieRelease.setText("Released : " +jsonArray.getString(4));
+                    mMovieRelease.setText("Release Date : " +jsonArray.getString(4));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
