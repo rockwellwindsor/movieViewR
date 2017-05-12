1a791b6ba1208d1dd6f5d5a7b57d30a54897098d
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-05-11 21:51:40 -0500

    Development: Passing movie id to review activity.
    Changes to be committed:
    	modified:   app/src/main/java/com/example/android/windsordesignstudio/movieviewr/DetailActivity.java
    	modified:   app/src/main/java/com/example/android/windsordesignstudio/movieviewr/ReviewActivity.java

--- app/src/main/java/com/example/android/windsordesignstudio/movieviewr/DetailActivity.java
+++ app/src/main/java/com/example/android/windsordesignstudio/movieviewr/DetailActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -110,11 +109,12 @@ public class DetailActivity extends AppCompatActivity {
 
     private void loadMovieReviews(String id) {
         Intent intent = new Intent(this, ReviewActivity.class);
+        intent.putExtra(Intent.EXTRA_TEXT, id);
         startActivity(intent);
 //        Log.d(TAG, "Load the movie that belongs to the id : " + id + "");
     }
 
     private void loadMovieTrailer(String id) {
-        Log.d(TAG, "Load the movie that belongs to the id : " + id + "");
+
     }
 }
--- app/src/main/java/com/example/android/windsordesignstudio/movieviewr/ReviewActivity.java
+++ app/src/main/java/com/example/android/windsordesignstudio/movieviewr/ReviewActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.windsordesignstudio.movieviewr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by rockwellrice on 5/11/17.
@@ -9,9 +11,16 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ReviewActivity extends AppCompatActivity {
 
+    private static final String TAG = ReviewActivity.class.getSimpleName();
+    public String mMovieID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        Intent intentThatStartedThisActivity = getIntent();
+        mMovieID = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+        Log.d(TAG, "HERE : " + mMovieID + "");
     }
 }
